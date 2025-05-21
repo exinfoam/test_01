ff85bce34cbd7b4d5455b1cc2ab72bbfeb4e6a97
exe <pickanmix@gmail.com>
2025-05-21 11:27:00 +0200

    Update ericsTable.js

--- ericsTable.js
+++ ericsTable.js
@@ -1,8 +1,6 @@
 let tbl2;
 
 function preload() {
-  // my table is comma separated value "csv"
-  // and has a header specifying the columns labels
   tbl2 = loadTable(
     'https://docs.google.com/spreadsheets/d/e/2PACX-1vSwfmixeG6WSFgvk66e0CZM0m6X3OqXfkwslm6QczrbfeqDo9lWNi7_YFi3iYwmEYMmMRB6qoWsC-4F/pub?gid=0&single=true&output=csv',
     'header',
@@ -13,7 +11,6 @@ function preload() {
 function setup() {
   noCanvas();
 
-  // Add custom CSS to control link styles
   const styleTag = createElement('style', `
     a.img-link {
       background-color: transparent !important;
@@ -26,12 +23,11 @@ function setup() {
   `);
   styleTag.parent(document.head || document.body);
 
-  // calling the function to display the p5.Table object as an HTML table
   build_HTML_table(tbl2, "tblabc", "myTable2", "w3-table-all");
 }
 
 function draw() {
-  // drawing something on the canvas so you can see it relative to the HTML table
+  // empty
 }
 
 function setValue(idTag, x) {
@@ -39,7 +35,6 @@ function setValue(idTag, x) {
 }
 
 function buildTable(columnHeader) {
-  // return an empty P5 table with headers but no data
   let t = new p5.Table();
   for (let c = 0; c < columnHeader.length; c++) {
     t.addColumn(columnHeader[c]);
@@ -55,14 +50,12 @@ function build_HTML_table(tbl, tableID, parentID, classID) {
   let imageColIndex = columnNames.indexOf("Images");
   let websiteColIndex = columnNames.indexOf("Website");
 
-  // setup the table header HTML string
   let hh = "<tr>";
   for (let c = 0; c < cc; c++) {
     hh += "<th>" + columnNames[c] + "</th>";
   }
   hh += "</tr>";
 
-  // setup the table row HTML string
   let rh = "";
   for (let r = 0; r < rc; r++) {
     rh += "<tr>";
@@ -72,7 +65,6 @@ function build_HTML_table(tbl, tableID, parentID, classID) {
       let websiteURL = websiteColIndex !== -1 ? tbl.get(r, websiteColIndex) : null;
       let cellContent = formatCellContent(cell, colName, websiteURL);
 
-      // Apply special styling for "Further notes and links"
       let cellStyle = "";
       if (colName === "Further notes and links") {
         cellStyle = ' style="max-width: 200px; word-wrap: break-word;"';
@@ -83,7 +75,6 @@ function build_HTML_table(tbl, tableID, parentID, classID) {
     rh += "</tr>";
   }
 
-  // create and insert the HTML table element
   let t = createElement('table', hh + rh);
   t.addClass(classID);
   t.id(tableID);
@@ -97,7 +88,6 @@ function formatCellContent(text, columnName, websiteURL) {
   if (text.match(/^https?:\/\/.*\.(jpeg|jpg|gif|png|svg|webp)(\?.*)?$/i)) {
     let imgTag = '<img src="' + text + '" style="max-height: 100px; width: auto; height: auto;">';
 
-    // If in "Images" column and website exists, wrap image in link
     if (columnName === "Images" && websiteURL && websiteURL.startsWith("http")) {
       return '<a href="' + websiteURL + '" target="_blank" class="img-link">' + imgTag + '</a>';
     } else {
@@ -105,9 +95,14 @@ function formatCellContent(text, columnName, websiteURL) {
     }
   }
 
-  // If it's a regular URL, make it clickable
+  // Make URLs clickable
   let urlRegex = /(\bhttps?:\/\/[^\s<>"]+[^\s<>.,;!"')\]])/g;
-  return text.replace(urlRegex, function (url) {
+  let formatted = text.replace(urlRegex, function (url) {
     return '<a href="' + url + '" target="_blank">' + url + '</a>';
   });
+
+  // ✅ NEW: Replace newline characters with <br> for HTML display
+  formatted = formatted.replace(/\n/g, "<br>");
+
+  return formatted;
 }
